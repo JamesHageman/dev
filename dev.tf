e6064012eb58b8b9d63a268b022131a2b7f91097
James Hageman <jamesd@hageman.ca>
2019-04-13 19:37:51 -0700

    basic terraform

--- dev.tf
+++ dev.tf
@@ -1,3 +1,15 @@
+variable "digitalocean_token" {}
+variable "ssh_public_key" {}
+variable "local_public_ip_address" {}
+
+variable "droplet_name" {
+  default = "dev1"
+}
+
+variable "region" {
+  default = "sfo2"
+}
+
 provider "digitalocean" {
   token = "${var.digitalocean_token}"
 }
@@ -10,42 +22,44 @@ resource "digitalocean_domain" "cloud" {
 # Create a new SSH key
 resource "digitalocean_ssh_key" "dev" {
   name       = "dev"
-  public_key = "${vars.ssh_public_key}"
+  public_key = "${var.ssh_public_key}"
 }
 
 # Create a new Droplet using the SSH key
-resource "digitalocean_droplet" "dev1" {
+resource "digitalocean_droplet" "devbox" {
   image    = "ubuntu-18-04-x64"
-  name     = "dev1.sfo2"
-  region   = "sfo2"
+  name     = "${var.droplet_name}.${var.region}"
+  region   = "${var.region}"
   size     = "s-1vcpu-1gb"
+  monitoring = true
   ssh_keys = ["${digitalocean_ssh_key.dev.fingerprint}"]
 }
 
 # Create a floating (static) ip for the droplet
-resource "digitalocean_floating_ip" "dev1" {
-  droplet_id = "${digitalocean_droplet.dev1.id}"
-  region     = "${digitalocean_droplet.dev1.region}"
+resource "digitalocean_floating_ip" "devbox_ip" {
+  droplet_id = "${digitalocean_droplet.devbox.id}"
+  region     = "${digitalocean_droplet.devbox.region}"
 }
 
 # Add an A record to point at the droplet
-resource "digitalocean_record" "dev1" {
-  domain = "${digitalocean_domain.default.name}"
+resource "digitalocean_record" "devbox_a_record" {
+  domain = "${digitalocean_domain.cloud.name}"
   type   = "A"
-  name   = "${digitalocean_droplet.dev1.name}"
-  value  = "${digitalocean_floating_ip.dev1.ip_address}"
+  ttl    = 60
+  name   = "${digitalocean_droplet.devbox.name}"
+  value  = "${digitalocean_floating_ip.devbox_ip.ip_address}"
 }
 
 resource "digitalocean_firewall" "laptop_ssh" {
   name = "laptop-ssh-access"
 
-  droplet_ids = ["${digitalocean_droplet.dev1.id}"]
+  droplet_ids = ["${digitalocean_droplet.devbox.id}"]
 
   inbound_rule = [
     {
       protocol           = "tcp"
       port_range         = "22"
-      source_addresses   = ["${vars.local_public_ip_address}"]
+      source_addresses   = ["${var.local_public_ip_address}"]
     },
   ]
 }
@@ -53,7 +67,7 @@ resource "digitalocean_firewall" "laptop_ssh" {
 resource "digitalocean_firewall" "public_egress" {
   name = "public-egress"
 
-  droplet_ids = ["${digitalocean_droplet.dev1.id}"]
+  droplet_ids = ["${digitalocean_droplet.devbox.id}"]
 
   outbound_rule = [
     {
@@ -73,3 +87,6 @@ resource "digitalocean_firewall" "public_egress" {
   ]
 }
 
+output "devbox_hostname" {
+  value = "${digitalocean_record.devbox_a_record.fqdn}"
+}
--- ssh.tfvars
+++
@@ -1,9 +0,0 @@
-variable "ssh_public_key" {
-  default="${file(~/.ssh/uw.pub)}"
-}
-
-# when switching wifi, you can find your public ip with this command:
-#   curl ifconfig.me
-variable "local_public_ip_address" {
-  default="24.56.243.216"
-}
