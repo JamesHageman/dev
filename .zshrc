63692f29ddcfe8e8cfdaef78900adc45995668cc
James Hageman <jamesd@hageman.ca>
2019-07-07 13:35:51 -0400

    add zsh plugins

--- .zshrc
+++ .zshrc
@@ -2,8 +2,10 @@ export ZSH="/root/.oh-my-zsh"
 
 ZSH_THEME="robbyrussell"
 
-plugins=(git)
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 
 export PROMPT="%F{magenta}%n%f@%F{blue}%m ${ret_status} %{$fg[cyan]%}%c%{$reset_color%} $(git_prompt_info)"
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
--- bootstrap.sh
+++ bootstrap.sh
@@ -12,10 +12,27 @@ function install_packages {
   done
 }
 
-install_packages jq mosh zsh
+install_packages jq tree mosh zsh
 
 locale-gen en_CA.UTF-8
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+zsh_plugins="$HOME/.oh-my-zsh/custom/plugins"
+autosuggestions="$zsh_plugins/zsh-autosuggestions"
+syntax_highlighting="$zsh_plugins/zsh-syntax-highlighting"
+
+if [ ! -d "$autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$autosuggestions"
+fi
+
+if [ ! -d "$syntax_highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$syntax_highlighting"
+fi
+
+if [ ! -d "$HOME/.fzf" ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME"/.fzf
+  "$HOME"/.fzf/install
 fi
